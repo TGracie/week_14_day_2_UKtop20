71e405cb16b1c2bfbe63200470131173d76f0bc2
Thomas Gracie <tgracie030@gmail.com>
2018-12-12 09:11:18 +0000

    marginally better

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,23 +1,23 @@
 import React, {Component, Fragment} from 'react';
 
-// const SongDetail = (props) => {
-//   if(!props) return null;
-//   return(
-//     <>
-//     <h2>Song Title: {this.props.name}</h2>
-//     <h3>AAAAAAHHHHHH</h3>
-//     </>
-//   )
-// }
-//
-// export default SongDetail;
-
-export default class SongDetail extends Component {
-  render(){
-    return(
-      <Fragment>
-        <ol>Song Title: {this.props.name}</ol>
-      </Fragment>
-    )
-  }
+const SongDetail = ({name}) => {
+  // if(!props) return null;
+  return(
+    <>
+    <h2>Song Title: {name}</h2>
+    <h3>AAAAAAHHHHHH</h3>
+    </>
+  )
 }
+
+export default SongDetail;
+
+// export default class SongDetail extends Component {
+//   render(){
+//     return(
+//       <Fragment>
+//         <ol>Song Title: {this.props.name}</ol>
+//       </Fragment>
+//     )
+//   }
+// }
